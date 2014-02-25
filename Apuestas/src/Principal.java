98e0ffdddfe0aca1b21a0ac7527a3b6498c560c8
Raul Garcia <satanin@gmail.com>
2014-02-25 11:00:26 +0100

    Main añadido

--- Apuestas/src/Principal.java
+++ Apuestas/src/Principal.java
@@ -1,8 +1,9 @@
 
 public class Principal {
 
-	public Principal() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
