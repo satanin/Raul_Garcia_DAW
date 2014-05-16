cf5796db92a436a2112da80c61bdc67411fc04fe
Raul Garcia Ruiz <satanin@gmail.com>
2014-05-16 12:59:02 +0200

    Añadiendo carpeta assets

--- Actividad Proyecto - Trivial/src/Principal.java
+++ Actividad Proyecto - Trivial/src/Principal.java
@@ -4,6 +4,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 
 public class Principal extends JFrame {
@@ -30,12 +36,39 @@ public class Principal extends JFrame {
 	 * Create the frame.
 	 */
 	public Principal() {
+		setTitle("Trivial Duels");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 599, 465);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Inicio");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmInicio = new JMenuItem("Practica Solo");
+		mnNewMenu.add(mntmInicio);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Duelo");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenu mnNewMenu_1 = new JMenu("Ayuda");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmAyuda = new JMenuItem("Como Jugar");
+		mnNewMenu_1.add(mntmAyuda);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Cr\u00E9ditos");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\RaulG\\git\\Raul_Garcia_DAW\\Actividad Proyecto - Trivial\\assets\\board.JPG"));
+		lblNewLabel.setBounds(0, 0, 583, 406);
+		contentPane.add(lblNewLabel);
 	}
-
 }
